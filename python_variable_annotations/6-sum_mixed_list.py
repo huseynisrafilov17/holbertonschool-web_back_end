4e814adc4e7ce77346fabf016bd1f26b959211b4
huseynisrafilov17 <huseynisrafilov17@gmail.com>
2024-05-01 17:00:07 +0400

    6th task

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Annotations"""
-from typing import Union
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     total = 0
     for i in mxd_lst:
         total += i
