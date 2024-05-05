707b8c07ce65e166ab8d2216259255752e48388c
huseynisrafilov17 <huseynisrafilov17@gmail.com>
2024-05-05 17:00:32 +0400

    1st task

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,7 +8,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """Collects 10 numbers"""
-    result = []
-    async for i in async_generator():
-        result.append(i)
+    result = [i for i in async_generator()]
     return result
