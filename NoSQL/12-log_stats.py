266ecf3a1038aab719b881fc7951de630da1ab36
huseynisrafilov17 <huseynisrafilov17@gmail.com>
2024-05-09 22:51:32 +0400

    12th task

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,15 +2,13 @@
 """Example"""
 from pymongo import MongoClient
 
-
-client = MongoClient('mongodb://127.0.0.1:27017')
-nginx = client.logs.nginx
-find = {"method": "GET", "path": "/status"}
-print(f"{nginx.count_documents({})} logs")
-print("Methods:")
-print(f'\tmethod GET: {nginx.count_documents({"method": "GET"})}')
-print(f'\tmethod POST: {nginx.count_documents({"method": "POST"})}')
-print(f'\tmethod PUT: {nginx.count_documents({"method": "PUT"})}')
-print(f'\tmethod PATCH: {nginx.count_documents({"method": "PATCH"})}')
-print(f'\tmethod DELETE: {nginx.count_documents({"method": "DELETE"})}')
-print(f'{nginx.count_documents(find)} status check')
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx = client.logs.nginx
+    find = {"method": "GET", "path": "/status"}
+    print(f"{nginx.count_documents({})} logs")
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        dictionary = {"method": method}
+        print(f'\tmethod {method}: {nginx.count_documents(dictionary)}')
+    print(f'{nginx.count_documents(find)} status check')
