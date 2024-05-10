b6d03183a382957ec8b9da2ec3b67b9d5b42493f
huseynisrafilov17 <huseynisrafilov17@gmail.com>
2024-05-10 09:23:49 +0400

    1st task

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -26,6 +26,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Gets pages"""
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
         pages = index_range(page, page_size)
