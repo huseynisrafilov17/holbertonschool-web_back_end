7595afb8cc929b688b381e4d2f16f5287aeb0cf7
huseynisrafilov17 <huseynisrafilov17@gmail.com>
2024-05-19 19:26:27 +0400

    3rd task

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -4,25 +4,24 @@ export default class Currency {
     this._name = name;
     this._code = code;
   }
-  
+
   get name() {
     return this._name;
   }
-  
+
   set name(value) {
     this._name = value;
   }
-  
+
   get code() {
     return this._code;
   }
-  
+
   set code(value) {
     this._code = value;
   }
 
   displayFullCurrency() {
-    return `${this.name} (${this.code})`
+    return `${this.name} (${this.code})`;
   }
 }
-  
